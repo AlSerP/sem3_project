72ef6171042fda2b2e875b5ff4e26164352ba40b
George Pogrebnoi <gosha1416@yandex.ru>
2021-10-17 12:47:29 +0300

    Emails are unique now

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     rating = models.IntegerField(default=0)
+
+    class Meta(object):
+        unique_together = ('email',)
